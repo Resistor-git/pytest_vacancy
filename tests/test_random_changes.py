5ba21fb91092d62c9b4b5f33b52865c6f7e1fa87
Resistor-git <partizan-resistor@yandex.ru>
2021-12-11 23:36:55 +0300

    added slow function to update Weapons table in tests

--- tests/test_random_changes.py
+++ tests/test_random_changes.py
@@ -5,8 +5,10 @@ import sqlite3
 from pathlib import Path
 from random import randint
 
+
 @pytest.fixture(scope='session')
 def copy_db():
+    """returns path for copy of the database"""
     # looks for wargaming.db in parent directory of test_random_changes.py
     path = Path('.')
     DATABASE_PATH = path / 'wargaming.db'
@@ -16,7 +18,7 @@ def copy_db():
     return db_copy_for_tst
 
 
-def test_change_weapon(copy_db):
+def test_change_ships_properties(copy_db):
     """takes the result of copy_db() and makes random changes in it"""
     # print("\nMYDEBUG", copy_db)
     # print("\nMYDEBUG", type(copy_db))
@@ -27,7 +29,7 @@ def test_change_weapon(copy_db):
     random_number = randint(1, 3)
     if random_number == 1:
         # change weapon
-        print(f"\nrandom_number = {random_number}, changing weapon")
+        print(f"\nrandom_number = {random_number}, changing weapon in Ships table")
 
         # list with each ship
         list_of_ships = []
@@ -54,7 +56,7 @@ def test_change_weapon(copy_db):
             cur.executemany("UPDATE Ships SET weapon = ? WHERE ship = ?", ship_weapon_tuple)
     elif random_number == 2:
         # change hull
-        print(f"\nrandom_number = {random_number}, changing hull")
+        print(f"\nrandom_number = {random_number}, changing hull in Ships table")
 
         list_of_ships = []
         for i in range(1, 201):
@@ -76,7 +78,7 @@ def test_change_weapon(copy_db):
     
     elif random_number == 3:
         # change engine
-        print(f"\nrandom_number = {random_number}, changing engine")
+        print(f"\nrandom_number = {random_number}, changing engine in Ships table")
 
         list_of_ships = []
         for i in range(1, 201):
@@ -95,9 +97,73 @@ def test_change_weapon(copy_db):
         # cur.executemany expects ONE list (or tuple) of tuples as a wildcard, like ('engine-5', 'ship-105')
         with con:
             cur.executemany("UPDATE Ships SET engine = ? WHERE ship = ?", ship_engine_tuple)
+        
+        assert 1 == 1
+
+# may be buggy; does it take the original copy or copy modified by test_change_ships_properties ???
+def test_change_weapons_hulls_engines_properties(copy_db):
+    """takes the result of copy_db() and makes random change in random property of each component (weapons, hulls, engines)"""
+    con = sqlite3.connect(copy_db)
+    cur = con.cursor()
+
+    # change columns in Weapons table
+    # attmept to make code a little more flexible, without hardcoded number of rows
+    # fetchall() returns a list of tuples, like [(15,)]
+    quantity_of_rows = cur.execute("SELECT COUNT(*) FROM Weapons").fetchall()[0][0]
+    #print("\n", quantity_of_rows)
+
+    random_number = randint(1, 4)
+    if random_number == 1:
+        # change Weapons
+        # list_of_weapons = []
+        print(f"\nrandom_number = {random_number}, changing properties in Weapons table")
+        for i in range(1, quantity_of_rows):
+            weapon = "weapon-" + str(i)
+            # list_of_weapons.append(weapon)
+
+            n = 1
+            # n = randint(1, 5)
+            if n == 1:
+                print(f"\nn = {n}, changing reload_speed in Weapons table")
+                with con:
+                    cur.execute("UPDATE Weapons SET reload_speed = :reload_speed WHERE weapon = :weapon", {"reload_speed" : randint(1, 20), "weapon" : weapon})
+            elif n == 2:
+                print(f"\nn = {n}, changing rotation_spee in Weapons table")
+                with con:
+                    cur.execute("UPDATE Weapons SET rotation_speed = :rotation_speed WHERE weapon = :weapon", {"rotation_speed" : randint(1, 20), "weapon" : weapon})
+            elif n == 3:
+                print(f"\nn = {n}, changing diameter in Weapons table")
+                with con:
+                    cur.execute("UPDATE Weapons SET diameter = :diameter WHERE weapon = :weapon", {"diameter" : randint(1, 20), "weapon" : weapon})
+            elif n == 4:
+                print(f"\nn = {n}, changing power_volley in Weapons table")
+                with con:
+                    cur.execute("UPDATE Weapons SET power_volley = :power_volley WHERE weapon = :weapon", {"power_volley" : randint(1, 20), "weapon" : weapon})
+            elif n == 5:
+                print(f"\nn = {n}, changing count in Weapons table")
+                with con:
+                    cur.execute("UPDATE Weapons SET count = :count WHERE weapon = :weapon", {"count" : randint(1, 20), "weapon" : weapon})
+
+        # weapons_properties = []
+        # for weapon in list_of_weapons:
+        #     # class instance: Weapon(weapon, reload_speed, rotation_speed, diameter, power_volley, count)
+        #     weapon_instance = Weapon(weapon, randint(1, 20), randint(1, 20), randint(1, 20), randint(1, 20), randint(1, 20))
+        #     print(weapon_instance)
+        #I CAN'T IMPORT my_classes, SO...
+
+
+
+        
+        # with con:
+        #     cur.executemany("UPDATE Weapons SET {column_name} = :column_value WHERE weapon = :weapon", {"column_value" : randint(1, 20), "weapon" : })
+    elif random_number == 2:
+        pass
+    elif random_number == 3:
+        pass
+    elif random_number == 4:
+        pass
 
 
 
 
     assert 1 == 1
-
