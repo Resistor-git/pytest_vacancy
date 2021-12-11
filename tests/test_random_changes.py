87d3a3178e668620010d293eeaa9db829ddf87c3
Resistor-git <partizan-resistor@yandex.ru>
2021-12-11 20:10:19 +0300

    test randomly changes weapon or hull or engine in Ships table

--- tests/test_random_changes.py
+++ tests/test_random_changes.py
@@ -23,32 +23,81 @@ def test_change_weapon(copy_db):
     con = sqlite3.connect(copy_db)
     cur = con.cursor()
 
-    # 200 random weapon parameters (one per each ship)
-    # list_of_weapons = []
+    # change either weapon or hull or engine, based on random_number
+    random_number = randint(1, 3)
+    if random_number == 1:
+        # change weapon
+        print(f"\nrandom_number = {random_number}, changing weapon")
+
+        # list with each ship
+        list_of_ships = []
+        for i in range(1, 201):
+            ship = "ship-" + str(i)
+            # print(ship)
+            list_of_ships.append(ship)
+
+        # 200 random weapon parameters (one per each ship)
+        list_of_weapons = []
+        for i in range(1, 201):
+            weapon = "weapon-" + str(randint(1, 20))
+            list_of_weapons.append(weapon)
+        #print("!!!!!", list_of_weapons)
+
+        # ship_weapon_tuple consists of tuples ('ship-x', number)
+        # used for cur.executemany
+        ship_weapon_tuple = list(zip(list_of_weapons, list_of_ships))
+        # print(ship_weapon_tuple)
+
+        # update randomized weapon for each ship (each row in table Ships)
+        # cur.executemany expects ONE list (or tuple) of tuples as a wildcard, like ('weapon-5', 'ship-105')
+        with con:
+            cur.executemany("UPDATE Ships SET weapon = ? WHERE ship = ?", ship_weapon_tuple)
+    elif random_number == 2:
+        # change hull
+        print(f"\nrandom_number = {random_number}, changing hull")
+
+        list_of_ships = []
+        for i in range(1, 201):
+            ship = "ship-" + str(i)
+            list_of_ships.append(ship)
+
+        list_of_hulls = []
+        for i in range(1, 201):
+            hull = "hull-" + str(randint(1, 20))
+            list_of_hulls.append(hull)
+
+        ship_hull_tuple = list(zip(list_of_hulls, list_of_ships))
+        #print(ship_hull_tuple)
+
+        # update randomized hull for each ship (each row in table Ships)
+        # cur.executemany expects ONE list (or tuple) of tuples as a wildcard, like ('hull-5', 'ship-105')
+        with con:
+            cur.executemany("UPDATE Ships SET hull = ? WHERE ship = ?", ship_hull_tuple)
     
-    rand_numbers_weapon = [randint(1, 20) for num in range(200)]
+    elif random_number == 3:
+        # change engine
+        print(f"\nrandom_number = {random_number}, changing engine")
 
-    # for i in range(1, 201):
-    #     weapon = "weapon-" + str(randint(1, 20))
-    #     list_of_weapons.append(weapon)
-    # print("!!!!!", list_of_weapons)
+        list_of_ships = []
+        for i in range(1, 201):
+            ship = "ship-" + str(i)
+            list_of_ships.append(ship)
 
-    # # cur.executemany expects list of tuples as a wildcard
-    # rand_numbers_tuples_weapon = [(num,) for num in rand_numbers_weapon]
-    #print(rand_numbers_tuples_weapon)
+        list_of_engines = []
+        for i in range(1, 201):
+            hull = "engine-" + str(randint(1, 20))
+            list_of_engines.append(hull)
 
-    list_of_ships = []
-    for i in range(1, 201):
-        ship = "ship-" + str(i)
-        # print(ship)
-        list_of_ships.append(ship)
-    # cur.executemany expects list of tuples as a wildcard (does it?)
-    # ship_weapon_tuple consists of tuples ('ship-x', number)
-    ship_weapon_tuple = tuple(zip(rand_numbers_weapon, list_of_ships))
-    #print(ship_weapon_tuple)
+        ship_engine_tuple = list(zip(list_of_engines, list_of_ships))
+        #print(ship_hull_tuple)
 
-    with con:
-        # randomize weapon for each ship (each row in table Ships)
-        cur.executemany("UPDATE Ships SET weapon = ? WHERE ship = ?", ship_weapon_tuple)
+        # update randomized engine for each ship (each row in table Ships)
+        # cur.executemany expects ONE list (or tuple) of tuples as a wildcard, like ('engine-5', 'ship-105')
+        with con:
+            cur.executemany("UPDATE Ships SET engine = ? WHERE ship = ?", ship_engine_tuple)
+
+
+
+
     assert 1 == 1
 
