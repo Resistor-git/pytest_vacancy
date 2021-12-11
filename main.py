740f98479c357f6b70e35b152eef4b6cd9758afb
Resistor-git <partizan-resistor@yandex.ru>
2021-12-11 17:04:23 +0300

    now database is created much faster: used 'executemany' instead of 'execute'

--- main.py
+++ main.py
@@ -48,30 +48,20 @@ def create_db():
 
 def insert_ships():
     """randomly insert values in table Ships"""
-    # class instance: Ship(ship, hull, engine, weapon)
-    # number_of_ships = list(range(1, 201))
-    # for num in number_of_ships:
-    #     rand_num_hull = randint(1, 5)
-    #     rand_num_engine = randint(1, 6)
-    #     rand_num_weapon = randint(1, 20)
-
-    #     ship_instance = Ship(f"ship-{num}", f"hull-{rand_num_hull}", f"engine-{rand_num_engine}", f"weapon-{rand_num_weapon}")
-    #     print(ship_instance)
-    #     with con:
-    #         cur.execute("INSERT INTO Ships (ship, hull, engine, weapon) VALUES (:ship, :hull, :engine, :weapon)",
-    #                     {"ship" : ship_instance.ship, "hull" : ship_instance.hull, "engine" : ship_instance.engine, "weapon" : ship_instance.weapon,})
-
+    # list_of_ships used in cur.executemany; contains tuples, each tuple represents a ship
     list_of_ships = []
+
     for num in list(range(1, 201)):
         rand_num_hull = randint(1, 5)
         rand_num_engine = randint(1, 6)
         rand_num_weapon = randint(1, 20)
+        # class instance: Ship(ship, hull, engine, weapon)
         ship_instance = Ship(f"ship-{num}", f"hull-{rand_num_hull}", f"engine-{rand_num_engine}", f"weapon-{rand_num_weapon}")
+        print(ship_instance)
 
+        # the_ship_params is a tuple, contains data about one ship
         the_ship_params = (ship_instance.ship, ship_instance.hull, ship_instance.engine, ship_instance.weapon)
-        print(the_ship_params)
         list_of_ships.append(the_ship_params)
-        #print(list_of_ships)
 
     with con:
         cur.executemany("INSERT INTO Ships (ship, hull, engine, weapon) VALUES (?, ?, ?, ?)", list_of_ships)
@@ -79,42 +69,61 @@ def insert_ships():
 
 def insert_weapons():
     """randomly insert values in table Weapons"""
-    # class instance: Weapon(weapon, reload_speed, rotation_speed, diameter, power_volley, count)
+    # list_of_weapons used in cur.executemany; contains tuples, each tuple represents a weapon
+    list_of_weapons = []
     number_of_weapons = list(range(1, 21))
+
     for num in number_of_weapons:
+        # class instance: Weapon(weapon, reload_speed, rotation_speed, diameter, power_volley, count)
         weapon_instance = Weapon(f"weapon-{num}", randint(1, 20), randint(1, 20), randint(1, 20),
                                 randint(1, 20), randint(1, 20))
         print(weapon_instance)
-        with con:
-            cur.execute("""INSERT INTO Weapons (weapon, reload_speed, rotation_speed, diameter, power_volley, count)
-                        VALUES (:weapon, :reload_speed, :rotation_speed, :diameter, :power_volley, :count)""",
-                        {"weapon" : weapon_instance.weapon, "reload_speed" : weapon_instance.reload_speed, "rotation_speed" : weapon_instance.rotation_speed,
-                        "diameter" : weapon_instance.diameter, "power_volley" : weapon_instance.power_volley, "count" : weapon_instance.count})
+
+        the_weapon_params = (weapon_instance.weapon, weapon_instance.reload_speed, weapon_instance.rotation_speed, weapon_instance.diameter, weapon_instance.power_volley, weapon_instance.count)
+        list_of_weapons.append(the_weapon_params)
+
+    with con:
+        cur.executemany("""INSERT INTO Weapons (weapon, reload_speed, rotation_speed, diameter, power_volley, count)
+                        VALUES (?, ?, ?, ?, ?, ?)""", list_of_weapons)
 
 
 def insert_hulls():
     """randomly insert values in table Hulls"""
-    # class instance: Hull(hull, armor, type, capacity)
+    # list_of_hulls used in cur.executemany; contains tuples, each tuple represents a hull
+    list_of_hulls = []
     number_of_hulls = list(range(1, 6))
+
     for num in number_of_hulls:
+        # class instance: Hull(hull, armor, type, capacity)
         hull_instance = Hull(f"hull-{num}", randint(1, 20), randint(1, 20), randint(1, 20))
         print(hull_instance)
-        with con:
-            cur.execute("""INSERT INTO Hulls (hull, armor, type, capacity) 
-                        VALUES (:hull, :armor, :type, :capacity)""",
-                        {"hull" : hull_instance.hull, "armor" : hull_instance.armor, "type" : hull_instance.type, "capacity" : hull_instance.capacity})
+
+        the_hull_params = (hull_instance.hull, hull_instance.armor, hull_instance.type, hull_instance.capacity)
+        list_of_hulls.append(the_hull_params)
+
+    with con:
+        cur.executemany("""INSERT INTO Hulls (hull, armor, type, capacity) 
+                        VALUES (?, ?, ?, ?)""", list_of_hulls)
 
 
 def insert_engines():
     """randomly insert values in table Engines"""
-    # class instance: Engine(engine, power, type)
+    # list_of_engines used in cur.executemany; contains tuples, each tuple represents an engine
+    list_of_engines = []
     number_of_engines = list(range(1, 7))
+
     for num in number_of_engines:
+        # class instance: Engine(engine, power, type)
         engine_instance = Engine(f"engine-{num}", randint(1, 20), randint(1, 20))
         print(engine_instance)
-        with con:
-            cur.execute("INSERT INTO Engines (engine, power, type) VALUES (:engine, :power, :type)",
-                        {"engine" : engine_instance.engine, "power" : engine_instance.power, "type" : engine_instance.type})
+
+        the_engine_params = (engine_instance.engine, engine_instance.power, engine_instance.type)
+        # print(the_engine_params)
+        list_of_engines.append(the_engine_params)
+
+
+    with con:
+        cur.executemany("INSERT INTO Engines (engine, power, type) VALUES (?, ?, ?)", list_of_engines)
 
 
 create_db()
