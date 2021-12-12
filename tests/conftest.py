88da03feabe9c3dec04a798fb8e7309d69593940
Resistor-git <partizan-resistor@yandex.ru>
2021-12-12 16:45:43 +0300

    changed function ' change_ships_properties'. Now it updates different prorerties for each ship (weapon for ship-1, engine for ship-2...), before it updated the same property for all ships

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,88 +23,53 @@ def copy_db():
 def change_ships_properties(copy_db):
     """changes columns weapon, hull, engine in Ships table;
     takes the result of copy_db() and makes random changes in it
-    'Для каждого корабля меняется на случайный один из компонентов: корпус, орудие или двигатель' - не уверен, что правильно понял. Я сделал так, что у всех кораблей
-    меняется одинаковый компонент: корпус, орудие или двигатель.
+    'Для каждого корабля меняется на случайный один из компонентов: корпус, орудие или двигатель'
     """
     # print("\nMYDEBUG", copy_db)
     # print("\nMYDEBUG", type(copy_db))
     con = sqlite3.connect(copy_db)
     cur = con.cursor()
 
-    # change either weapon or hull or engine, based on random_number
-    random_number = randint(1, 3)
+    # list with each ship
+    list_of_ships = []
+    for i in range(1, 201):
+        ship = "ship-" + str(i)
+        # print(ship)
+        list_of_ships.append(ship)
+
+    # each list contains ships with changed properties, one list per property [('weapon-19', 'ship-1'), ('weapon-5', 'ship-22', )...]; [('hull-3', 'ship-4'), ('hull-5', 'ship-7', )...]
+    list_of_weapon_changes = []
+    list_of_hull_changes = []
+    list_of_engine_changes = []
+
+    # change either weapon or hull or engine of each ship, based on random_number
+    for ship in list_of_ships:
+        random_number = randint(1, 3)
+        if random_number == 1:
+            # change weapon
+            print(f"\nrandom_number = {random_number}, changing weapon in Ships table for {ship}")
+            new_weapon = "weapon-" + str(randint(1, 20))
+            list_of_weapon_changes.append((new_weapon, ship))
+        elif random_number == 2:
+            # change hull
+            print(f"\nrandom_number = {random_number}, changing hull in Ships table for {ship}")
+            new_hull = "hull-" + str(randint(1, 20))
+            list_of_hull_changes.append((new_hull, ship))
+        elif random_number == 3:
+            # change engine
+            print(f"\nrandom_number = {random_number}, changing engine in Ships table for {ship}")
+            new_engine = "engine-" + str(randint(1, 20))
+            list_of_engine_changes.append((new_engine, ship))
+    # print(list_of_weapon_changes)
+    # print(list_of_hull_changes)
+    # print(list_of_engine_changes)
     
-    if random_number == 1:
-        # change weapon
-        print(f"\nrandom_number = {random_number}, changing weapon in Ships table")
-
-        # list with each ship
-        list_of_ships = []
-        for i in range(1, 201):
-            ship = "ship-" + str(i)
-            # print(ship)
-            list_of_ships.append(ship)
-
-        # 200 random weapon parameters (one per each ship)
-        list_of_weapons = []
-        for i in range(1, 201):
-            weapon = "weapon-" + str(randint(1, 20))
-            list_of_weapons.append(weapon)
-        #print("!!!!!", list_of_weapons)
-
-        # ship_weapon_tuple consists of tuples ('ship-x', number)
-        # used for cur.executemany
-        ship_weapon_tuple = list(zip(list_of_weapons, list_of_ships))
-        # print(ship_weapon_tuple)
-
-        # update randomized weapon for each ship (each row in table Ships)
-        # cur.executemany expects ONE list (or tuple) of tuples as a wildcard, like ('weapon-5', 'ship-105')
-        with con:
-            cur.executemany("UPDATE Ships SET weapon = ? WHERE ship = ?", ship_weapon_tuple)
-    
-    elif random_number == 2:
-        # change hull
-        print(f"\nrandom_number = {random_number}, changing hull in Ships table")
-
-        list_of_ships = []
-        for i in range(1, 201):
-            ship = "ship-" + str(i)
-            list_of_ships.append(ship)
-
-        list_of_hulls = []
-        for i in range(1, 201):
-            hull = "hull-" + str(randint(1, 20))
-            list_of_hulls.append(hull)
-
-        ship_hull_tuple = list(zip(list_of_hulls, list_of_ships))
-        #print(ship_hull_tuple)
-
-        # update randomized hull for each ship (each row in table Ships)
-        # cur.executemany expects ONE list (or tuple) of tuples as a wildcard, like ('hull-5', 'ship-105')
-        with con:
-            cur.executemany("UPDATE Ships SET hull = ? WHERE ship = ?", ship_hull_tuple)
-
-    elif random_number == 3:
-        # change engine
-        print(f"\nrandom_number = {random_number}, changing engine in Ships table")
-
-        list_of_ships = []
-        for i in range(1, 201):
-            ship = "ship-" + str(i)
-            list_of_ships.append(ship)
-
-        list_of_engines = []
-        for i in range(1, 201):
-            hull = "engine-" + str(randint(1, 20))
-            list_of_engines.append(hull)
-
-        ship_engine_tuple = list(zip(list_of_engines, list_of_ships))
-        #print(ship_hull_tuple)
-
-        # update randomized engine for each ship (each row in table Ships)
-        # cur.executemany expects ONE list (or tuple) of tuples as a wildcard, like ('engine-5', 'ship-105')
-        with con:
-            cur.executemany("UPDATE Ships SET engine = ? WHERE ship = ?", ship_engine_tuple)
-        
-        # assert 1 == 1
-
+    # write changed weapon in Ships table
+    with con:
+        cur.executemany(f"UPDATE Ships SET weapon = ? WHERE ship = ?", list_of_weapon_changes)
+    # write changed hull in Ships table
+    with con:
+        cur.executemany(f"UPDATE Ships SET hull = ? WHERE ship = ?", list_of_hull_changes)
+    # write changed engine in Ships table
+    with con:
+        cur.executemany(f"UPDATE Ships SET engine = ? WHERE ship = ?", list_of_engine_changes)
