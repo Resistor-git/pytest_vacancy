f94234975fb2b4bed3a9fdc7e9bc25dba0be020c
Resistor-git <partizan-resistor@yandex.ru>
2021-12-12 20:09:41 +0300

    test_what_changed_in_ships now shows difference in Ships table (between original and temporary databases)

--- tests/test_random_changes.py
+++ tests/test_random_changes.py
@@ -1,28 +1,47 @@
-import shutil
-import pytest
 import sqlite3
 
-from pathlib import Path
 from random import randint
 
 
 def test_what_changed_in_ships(copy_db, original_db):
     """Checks if weapon or hull or engine was changed for each ship in table Ships"""
-    # connection to original table
-    # con_orig = ???
+    # connection to original database
+    con_orig = sqlite3.connect(original_db)
+    cur_orig = con_orig.cursor()
+
+    data_orig = cur_orig.execute("SELECT * FROM Ships").fetchall()
+    # print("original", data_orig)
 
     # connection to temporary copy with changes
     con_temp = sqlite3.connect(copy_db)
     cur_temp = con_temp.cursor()
 
-    foo = cur_temp.execute("SELECT * FROM Ships WHERE ship = :ship", {"ship" : 'ship-1'}).fetchall()
-    print("copy:", foo)
+    data_copy = cur_temp.execute("SELECT * FROM Ships").fetchall()
+    # print("copy:", data_copy)
 
-    # connection to original database
-    con_orig = sqlite3.connect(original_db)
-    cur_orig = con_orig.cursor()
 
-    bar = cur_orig.execute("SELECT * FROM Ships WHERE ship = :ship", {"ship" : 'ship-1'}).fetchall()
-    print("original", bar)
+    # create list with all differences between original and modified databases
+    # differences is a list of tuples; each tuple looks like (('ship-200', 'weapon-20', 'hull-3', 'engine-6'), ('ship-200', 'weapon-20', 'hull-3', 'engine-7'))
+    i = 0
+    differences = []
+    for row in data_orig:
+        differences.append((data_orig[i], data_copy[i]))
+        i += 1
+    # print(differences)
+
+
+    for difference in differences:
+        # difference is tuple of tuples; each tuple looks like (('ship-200', 'weapon-20', 'hull-3', 'engine-6'), ('ship-200', 'weapon-20', 'hull-3', 'engine-7'))
+        # print(difference)
+        # print('original', difference[0])
+        # print('copy', difference[1])
+        # compare each element of tuple to another element of tuple, in other words compare every property of ship from original db with every property of ship from temporary db
+        for n in range(0, len(differences[0][0])):
+            if difference[0][n] != difference[1][n]:
+                # Ship-x, property-y (data from temp db)
+                #   expected property-z (from original db), was property-y (from temp db)
+                # difference[0] - original, difference[1] - temporary
+                print(f"""{difference[1][0]}, {difference[1][n]}\n    expected {difference[0][n]}, was {difference[1][n]}""")
+    
 
     assert 11==11
